2daaa2a52d165d8092d9ecc8671cab716144df34
Eduard Khiaev <eddietheone1984@gmail.com>
2024-07-04 12:47:16 +0300

    phonebook_app_urls

--- phonebook/urls.py
+++ phonebook/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from phonebook_app import urls as phonebook_app_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('phonebook_app.urls')),
+    path('', include(phonebook_app_urls)),
 ]
